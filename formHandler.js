aaec9ae33035aaeeb04bd6f44a718e6a2653fa3a
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-07-13 12:51:10 +0600

    api made for verifying otp

--- formHandler.js
+++ formHandler.js
@@ -6,6 +6,8 @@ const FormData = mongoose.model('FormData', formSchema);
 const OTPVerification = require('./models/OTPVerification');
 const nodemailer = require('nodemailer');
 const bcrypt = require("bcrypt");
+// const emailSchema = require('./models/emailSchema');
+// const EmailData = mongoose.model('EmailData', emailSchema);
 
 
 
@@ -43,52 +45,103 @@ router.post('/', async (req, res) => {
         }
     });
 
-    // try {
-    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
-    const mailOptions = {
-        from: '"Mohammad Jahid" <mohammadjahid0007@gmail.com>',
-        to: email,
-        subject: "Verify your email",
-        html: `<p>User <b>${otp}</b> to verify your mail</p>
+});
+
+//send the otp
+router.post('/emailOtp', async (req, res) => {
+    try {
+        const newData = req.body;
+        console.log(newData);
+        const email = newData.email;
+        console.log(email);
+        const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+        const mailOptions = {
+            from: '"Mohammad Jahid" <mohammadjahid0007@gmail.com>',
+            to: email,
+            subject: "Verify your email",
+            html: `<p>User <b>${otp}</b> to verify your mail</p>
             <p>This OTP expires in 1hour</p>
             `
+        }
+        // hash the otp
+        // const hashedOTP = await bcrypt.hash(otp, 10);
+        const newOTPVerification = await new OTPVerification({
+            otp,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 3600000
+        });
+        //save otp record
+        await newOTPVerification.save();
+        transporter.sendMail(mailOptions);
+        res.json({
+            status: "pending",
+            message: "OTP sent to your email",
+            data: {
+                email,
+            }
+        })
+        let info = await transporter.sendMail(mailOptions);
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     }
-    // hash the otp
-    // const hashedOTP = await bcrypt.hash(otp, 10);
-    // const newOTPVerification = await new OTPVerification({
-    //     otp: hashedOTP,
-    //     createdAt: Date.now(),
-    //     expiresAt: Date.now() + 3600000
-    // });
-    // //save otp record
-    // await newOTPVerification.save();
-    // transporter.sendMail(mailOptions);
-    // res.json({
-    //     status: "pending",
-    //     message: "OTP sent to your email",
-    //     data: {
-    //         email,
-    //     }
-    // })
-    let info = await transporter.sendMail(mailOptions);
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    // }
-
-
-
-    // catch (err) {
-    //     res.status(500).json({
-    //         status: "Failer",
-    //         message: err.message
-    //     })
-    // }
+    catch (err) {
+        res.status(500).json({
+            status: "Failed",
+            message: err.message
+        })
+    }
 });
+
+//verify the otp
+router.post('/verifyOtp', async (req, res) => {
+    try {
+        console.log(req.body);
+        let { otp } = req.body;
+        if (!otp) {
+            res.status(400).json({
+                status: "Failed",
+                message: "OTP is required"
+            })
+        } else {
+            const otpVerification = await OTPVerification.findOne({
+                // _id: id,
+                otp: otp,
+                expiresAt: { $gt: Date.now() }
+            });
+            console.log(otpVerification);
+            if (!otpVerification) {
+                res.status(400).json({
+                    status: "Failed",
+                    message: "OTP is invalid"
+                })
+            } else {
+                const validOTP = (otp == otpVerification.otp); //await bcrypt.compare(otp, otpVerification.otp);
+                if (!validOTP && validOTP.length !== 4) {
+                    res.status(400).json({
+                        status: "Failed",
+                        message: "OTP is invalid"
+                    })
+                } else {
+                    res.status(200).json({
+                        status: "success",
+                        message: "OTP is valid"
+                    })
+                }
+            }
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            status: "Failed",
+            message: err.message
+        })
+    }
+})
 
 
 
--- models/OTPVerification.js
+++ models/OTPVerification.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OTPVerificationSchema = new Schema({
-    otp: String,
+    otp: Number,
     createdAt: Date,
     expiresAt: Date,
 })
